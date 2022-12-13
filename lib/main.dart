9392cf3eee949dff6ef4ec1bbb1eb643aedb19a2
lalit2095 <31661583+lalit2095@users.noreply.github.com>
2022-12-13 19:13:38 +0530

    Day-1 first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+
+    return MaterialApp(
+        home: Material(
+            child: Center(
+                child: Container(
+      child: Text("Welcom to flutter Tutorials in $days Days."),
+    ))));
+  }
+}
